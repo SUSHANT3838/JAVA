e9e78a9ae76c260455c4ae4c9817edcb952a3a45
SUSHANT3838 <sushantshete876@gmail.com>
2026-02-25 14:30:23 +0530

    Variable

---
+++ introduction/variable.java
@@ -0,0 +1,16 @@
+public class variable {
+    public static void main(String[] args) {
+        // Data type -> To store the value
+        byte a = 10;
+        short b = 20;
+        char c = 'A';
+        boolean d = true;
+        int e = 50;
+        long f = 100000000000L;
+        float g = 35.72f;
+        double h = 2000;
+
+        
+        System.out.print(a +"\n"+ b+"\n"+ c+"\n"+ d+"\n"+ e+"\n"+ f+"\n"+ g+"\n"+ h);
+    }
+}
