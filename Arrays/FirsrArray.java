fea611408361e2dfc4b519dec1c331fdbe5e9e98
SUSHANT3838 <sushantshete876@gmail.com>
2026-02-25 18:08:26 +0530

    Array

---
+++ Arrays/FirsrArray.java
@@ -0,0 +1,9 @@
+public class FirsrArray {
+    public static void main(String[] args) {
+        String Student[] = new String[10];
+        int marks[] = new int[10];
+        Student[0] = "Sushant Shete";
+        marks[0] = 75;
+        System.out.print(Student[0] + " : " + marks[0]);
+    }
+}
